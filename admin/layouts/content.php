23f8f76254a0cbda19a3dd26be1968f811b73910
Supapanssh <m.supapan@kkumail.com>
2020-11-03 22:14:03 +0700

    merge to upload

--- admin/layouts/content.php
+++ admin/layouts/content.php
@@ -14,17 +14,6 @@
                         ?>
                     </h1>
                 </div><!-- /.col -->
-<<<<<<< HEAD
-=======
-                <div class="col-sm-6">
-                    <nav aria-label="breadcrumb">
-                        <ol id="w0" class="float-sm-right breadcrumb">
-                            <li class="breadcrumb-item"><a href="/admin-stock/web/">Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page"><?= $this->title ?></li>
-                        </ol>
-                    </nav>
-                </div>
->>>>>>> 3c8b23862db54ab49d0660683353f8ff554edc96
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
