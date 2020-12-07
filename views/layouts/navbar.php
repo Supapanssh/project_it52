735e31b0dc528902866cb4f42cc1061e59cdf282
Mr.Supakorn Emch <supagonava@gmail.com>
2020-12-07 23:16:59 +0700

    fix nav layout

--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -14,49 +14,26 @@ use yii\helpers\Url;
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-            <form method="get" action="<?= Url::to(['site/signout']) ?>">
-                <button type="submit" class="btn btn-default btn-flat float-right">ออกจากระบบ</button>
-            </form>
-            <!-- <?= Html::a('<i class="fas fa-sign-out-alt"></i>', ['site/Logout'], ['data-method' => 'post', 'class' => 'nav-link']) ?> -->
+
         </li>
         <li class="nav-item dropdown user-menu">
             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                <span class="d-none d-md-inline"><?= !Yii::$app->user->isGuest ? Yii::$app->user->identity->username : null ?></span>
+                <span class="d-none d-md-inline"><i class="fa fa-user-circle" aria-hidden="true"></i> <?= !Yii::$app->user->isGuest ? Yii::$app->user->identity->username : null ?></span>
             </a>
             <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <!-- User image -->
                 <li class="user-header bg-primary">
-                    <img src="<?= Yii::getAlias('@web'); ?>/assets/images/admin.png" class="img-circle elevation-2" alt="User Image">
-
+                    <img src="<?= Yii::getAlias('@web'); ?>/images/admin.png" class="img-circle elevation-2" alt="User Image">
                     <p>
-                        <?= !Yii::$app->user->isGuest ? Yii::$app->user->identity->username : null ?>
-                        <!-- <small>Member since Nov. 2012</small> -->
+                       <i class="fa fa-user-circle" aria-hidden="true"></i> <?= !Yii::$app->user->isGuest ? Yii::$app->user->identity->username : null ?>
                     </p>
                 </li>
-                <!-- Menu Body -->
-                <li class="user-body">
-                    <!-- <div class="row">
-                        <div class="col-4 text-center">
-                            <a href="#">Followers</a>
-                        </div>
-                        <div class="col-4 text-center">
-                            <a href="#">Sales</a>
-                        </div>
-                        <div class="col-4 text-center">
-                            <a href="#">Friends</a>
-                        </div>
-                    </div> -->
-                    <!-- /.row -->
-                </li>
-                <!-- Menu Footer-->
-                <li class="user-footer">
-                    <a href="#" class="btn btn-default btn-flat">Profile</a>
-
-                </li>
+                <div class="p-1">
+                    <form method="get" action="<?= Url::to(['site/signout']) ?>">
+                        <button type="submit" class="btn btn-default btn-flat float-right w-100 p-2">ออกจากระบบ</button>
+                    </form>
+                </div>
             </ul>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button"><i class="fas fa-th-large"></i></a>
+
         </li>
     </ul>
 </nav>
