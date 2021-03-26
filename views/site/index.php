8cf5c51b9596811f7f35eb44e2a2dd5513ddfd5e
Supapanssh <m.supapan@kkumail.com>
2021-03-26 23:12:37 +0700

    db

--- views/site/index.php
+++ views/site/index.php
@@ -215,10 +215,12 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
             </div>
 
         </div>
-        <div class="col-12">
-            <div class="col-md-8 " style="margin-top: 1rem;margin-bottom: 1rem;">
-                <div id="container4">
-                </div>
+        <div class="row ">
+            <div class="col" style="margin-top: 1rem;margin-bottom: 1rem;">
+                <div id="container4"></div>
+                <div id="container5"></div>
+                <div id="container6"></div>
+
             </div>
         </div>
 
@@ -554,216 +556,170 @@ var profitChart = Highcharts.chart('profit-chart', {
             });
             </script>
 
+
             <script src="https://code.highcharts.com/highcharts.js"></script>
-            <script src="https://code.highcharts.com/highcharts-more.js"></script>
-            <script src="https://code.highcharts.com/modules/solid-gauge.js"></script>
+            <script src="https://code.highcharts.com/modules/bullet.js"></script>
             <script src="https://code.highcharts.com/modules/exporting.js"></script>
             <script src="https://code.highcharts.com/modules/export-data.js"></script>
             <script src="https://code.highcharts.com/modules/accessibility.js"></script>
 
             <figure class="highcharts-figure">
-
+                <div id="container4"></div>
+                <div id="container5"></div>
+                <div id="container6"></div>
+                <p class="highcharts-description">
+                    A series of bullet charts showing Revenue, Profit, and New Customers.
+                    Bullet charts allow readers to quickly compare a single value to a
+                    target value. This demo is also using plot bands to separate different
+                    ranges on the axes. Bullet charts are popular in dashboards, as they
+                    typically take up less space than gauge charts.
+                </p>
             </figure>
 
 
-
-
-
             <div class="col-md-6 col-sm-12 " style="margin-top: 1rem;margin-bottom: 1rem;">
-                <div id="container4" style="max-width: 440px;height: 400px;margin: 0px auto">
-                </div>
+
                 <script>
-                // Uncomment to style it like Apple Watch
-                /*
-                if (!Highcharts.theme) {
-                  Highcharts.setOptions({
+                Highcharts.setOptions({
                     chart: {
-                      backgroundColor: 'black'
+                        inverted: true,
+                        marginLeft: 135,
+                        type: 'bullet'
                     },
-                    colors: ['#F62366', '#9DFF02', '#0CCDD6'],
                     title: {
-                      style: {
-                        color: 'silver'
-                      }
+                        text: null
                     },
-                    tooltip: {
-                      style: {
-                        color: 'silver'
-                      }
+                    legend: {
+                        enabled: false
+                    },
+                    yAxis: {
+                        gridLineWidth: 0
+                    },
+                    plotOptions: {
+                        series: {
+                            pointPadding: 0.25,
+                            borderWidth: 0,
+                            color: '#000',
+                            targetOptions: {
+                                width: '200%'
+                            }
+                        }
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    exporting: {
+                        enabled: false
                     }
-                  });
-                }
-                // */
-
-                /**
-                 * In the chart render event, add icons on top of the circular shapes
-                 */
-                function renderIcons() {
-
-                    // Move icon
-                    if (!this.series[0].icon) {
-                        this.series[0].icon = this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
-                            .attr({
-                                stroke: '#303030',
-                                'stroke-linecap': 'round',
-                                'stroke-linejoin': 'round',
-                                'stroke-width': 2,
-                                zIndex: 10
-                            })
-                            .add(this.series[2].group);
-                    }
-                    this.series[0].icon.translate(
-                        this.chartWidth / 2 - 10,
-                        this.plotHeight / 2 - this.series[0].points[0].shapeArgs.innerR -
-                        (this.series[0].points[0].shapeArgs.r - this.series[0].points[0].shapeArgs.innerR) / 2
-                    );
-
-                    // Exercise icon
-                    if (!this.series[1].icon) {
-                        this.series[1].icon = this.renderer.path(
-                                ['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8,
-                                    'M', 8, -8, 'L', 16, 0, 8, 8
-                                ]
-                            )
-                            .attr({
-                                stroke: '#ffffff',
-                                'stroke-linecap': 'round',
-                                'stroke-linejoin': 'round',
-                                'stroke-width': 2,
-                                zIndex: 10
-                            })
-                            .add(this.series[2].group);
-                    }
-                    this.series[1].icon.translate(
-                        this.chartWidth / 2 - 10,
-                        this.plotHeight / 2 - this.series[1].points[0].shapeArgs.innerR -
-                        (this.series[1].points[0].shapeArgs.r - this.series[1].points[0].shapeArgs.innerR) / 2
-                    );
-
-                    // Stand icon
-                    if (!this.series[2].icon) {
-                        this.series[2].icon = this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
-                            .attr({
-                                stroke: '#303030',
-                                'stroke-linecap': 'round',
-                                'stroke-linejoin': 'round',
-                                'stroke-width': 2,
-                                zIndex: 10
-                            })
-                            .add(this.series[2].group);
-                    }
-
-                    this.series[2].icon.translate(
-                        this.chartWidth / 2 - 10,
-                        this.plotHeight / 2 - this.series[2].points[0].shapeArgs.innerR -
-                        (this.series[2].points[0].shapeArgs.r - this.series[2].points[0].shapeArgs.innerR) / 2
-                    );
-                }
+                });
 
                 Highcharts.chart('container4', {
-
                     chart: {
-                        type: 'solidgauge',
-                        height: '110%',
-                        events: {
-                            render: renderIcons
-                        }
+                        marginTop: 40
                     },
-
                     title: {
-                        text: 'Activity',
-                        style: {
-                            fontSize: '24px'
-                        }
+                        text: '2017 YTD'
                     },
-
-                    tooltip: {
-                        borderWidth: 0,
-                        backgroundColor: 'none',
-                        shadow: false,
-                        style: {
-                            fontSize: '16px'
-                        },
-                        valueSuffix: '%',
-                        pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}</span>',
-                        positioner: function(labelWidth) {
-                            return {
-                                x: (this.chart.chartWidth - labelWidth) / 2,
-                                y: (this.chart.plotHeight / 2) + 15
-                            };
-                        }
+                    xAxis: {
+                        categories: ['<span class="hc-cat-title">Revenue</span><br/>U.S. $ (1,000s)']
                     },
-
-                    pane: {
-                        startAngle: 0,
-                        endAngle: 360,
-                        background: [{ // Track for Move
-                            outerRadius: '112%',
-                            innerRadius: '88%',
-                            backgroundColor: Highcharts.color(Highcharts.getOptions().colors[0])
-                                .setOpacity(0.3)
-                                .get(),
-                            borderWidth: 0
-                        }, { // Track for Exercise
-                            outerRadius: '87%',
-                            innerRadius: '63%',
-                            backgroundColor: Highcharts.color(Highcharts.getOptions().colors[1])
-                                .setOpacity(0.3)
-                                .get(),
-                            borderWidth: 0
-                        }, { // Track for Stand
-                            outerRadius: '62%',
-                            innerRadius: '38%',
-                            backgroundColor: Highcharts.color(Highcharts.getOptions().colors[2])
-                                .setOpacity(0.3)
-                                .get(),
-                            borderWidth: 0
-                        }]
-                    },
-
                     yAxis: {
-                        min: 0,
-                        max: 100,
-                        lineWidth: 0,
-                        tickPositions: []
+                        plotBands: [{
+                            from: 0,
+                            to: 150,
+                            color: '#666'
+                        }, {
+                            from: 150,
+                            to: 225,
+                            color: '#999'
+                        }, {
+                            from: 225,
+                            to: 9e9,
+                            color: '#bbb'
+                        }],
+                        title: null
                     },
-
-                    plotOptions: {
-                        solidgauge: {
-                            dataLabels: {
-                                enabled: false
-                            },
-                            linecap: 'round',
-                            stickyTracking: false,
-                            rounded: true
-                        }
-                    },
-
                     series: [{
-                        name: 'Move',
                         data: [{
-                            color: Highcharts.getOptions().colors[0],
-                            radius: '112%',
-                            innerRadius: '88%',
-                            y: 80
+                            y: 275,
+                            target: 250
                         }]
-                    }, {
-                        name: 'Exercise',
+                    }],
+                    tooltip: {
+                        pointFormat: '<b>{point.y}</b> (with target at {point.target})'
+                    }
+                });
+
+                Highcharts.chart('container5', {
+                    xAxis: {
+                        categories: ['<span class="hc-cat-title">Profit</span><br/>%']
+                    },
+                    yAxis: {
+                        plotBands: [{
+                            from: 0,
+                            to: 20,
+                            color: '#666'
+                        }, {
+                            from: 20,
+                            to: 25,
+                            color: '#999'
+                        }, {
+                            from: 25,
+                            to: 100,
+                            color: '#bbb'
+                        }],
+                        labels: {
+                            format: '{value}%'
+                        },
+                        title: null
+                    },
+                    series: [{
                         data: [{
-                            color: Highcharts.getOptions().colors[1],
-                            radius: '87%',
-                            innerRadius: '63%',
-                            y: 65
+                            y: 22,
+                            target: 27
                         }]
-                    }, {
-                        name: 'Stand',
+                    }],
+                    tooltip: {
+                        pointFormat: '<b>{point.y}</b> (with target at {point.target})'
+                    }
+                });
+
+
+                Highcharts.chart('container6', {
+                    xAxis: {
+                        categories: ['<span class="hc-cat-title">New Customers</span><br/>Count']
+                    },
+                    yAxis: {
+                        plotBands: [{
+                            from: 0,
+                            to: 1400,
+                            color: '#666'
+                        }, {
+                            from: 1400,
+                            to: 2000,
+                            color: '#999'
+                        }, {
+                            from: 2000,
+                            to: 9e9,
+                            color: '#bbb'
+                        }],
+                        labels: {
+                            format: '{value}'
+                        },
+                        title: null
+                    },
+                    series: [{
                         data: [{
-                            color: Highcharts.getOptions().colors[2],
-                            radius: '62%',
-                            innerRadius: '38%',
-                            y: 50
+                            y: 1650,
+                            target: 2100
                         }]
-                    }]
+                    }],
+                    tooltip: {
+                        pointFormat: '<b>{point.y}</b> (with target at {point.target})'
+                    },
+                    credits: {
+                        enabled: true
+                    }
                 });
                 </script>
 
