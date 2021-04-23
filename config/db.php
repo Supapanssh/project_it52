ddec379eab528f6f1af8f67a4c9328cfd377c006
Supapanssh <m.supapan@kkumail.com>
2021-04-23 16:20:23 +0700

    2021-04-23

--- config/db.php
+++ config/db.php
@@ -3,30 +3,10 @@ $whitelist = array(
     '127.0.0.1',
     '::1'
 );
-if (!in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
-    return [
-        'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=stock',
-        'username' => 'root',
-        'password' => 'root',
-        'charset' => 'utf8',
-
-        // Schema cache options (for production environment)
-        //'enableSchemaCache' => true,
-        //'schemaCacheDuration' => 60,
-        //'schemaCache' => 'cache',
-    ];
-} else {
-    return [
-        'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=stock',
-        'username' => 'root',
-        'password' => '',
-        'charset' => 'utf8',
-
-        // Schema cache options (for production environment)
-        //'enableSchemaCache' => true,
-        //'schemaCacheDuration' => 60,
-        //'schemaCache' => 'cache',
-    ];
-}
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=stock',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
